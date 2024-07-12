3b47261ef5189f7af7902b7b38337c2efc7d7b6d
Henrik Au-Young <auhenrik@gmail.com>
2024-07-13 07:00:32 +0800

    Header, Navs, Framer transitions added

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,12 +3,15 @@ import "./globals.css";
 
 // components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
-const jetbrainsMono = JetBrains_Mono({ 
-  subsets: ["latin"], 
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   variable: '--font-jetbrainsMono',
- });
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,8 +23,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}
-        </body>
+        <StairTransition />
+        <PageTransition>
+          {children}
+        </PageTransition>
+
+      </body>
     </html>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,34 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+// components
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
 const Header = () => {
     return (
-        <header className= "py-8 xl:py-12 text-white">
-            <div className="container mx-auto">
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
                 {/* Logo */}
-                <Link href= "/"> 
+                <Link href="/">
                     <h1 className="text-4xl font-semibold">
                         Henrik <span className="text-accent">.</span>
                     </h1>
                 </Link>
-                {/*Desktop Nav */}
+
+
+                {/* desktop nav and hire me button */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button> Hire me </Button>
+                    </Link>
+                </div>
+
+                {/* mobile nav */}
+                <div className="xl:hidden">
+                    <MobileNav />
+                </div>
             </div>
         </header>
     );
