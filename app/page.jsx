dc05006216befa64663e07dc71b5f40e63973026
Henrik Au-Young <auhenrik@gmail.com>
2024-07-13 14:31:47 +0800

    home page work. socials, photo, button.

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,39 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
-  return <main>homepage</main>;
-}
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Student Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Henrik Au-Young</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Aspiring software developer; proficient in a range of programming languages and technologies.
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div>socials</div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
