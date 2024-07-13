dfcde40fb74ca44788dd91ffee02b4db199837d8
Henrik Au-Young <auhenrik@gmail.com>
2024-07-13 15:47:35 +0800

    fixed ' exception

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
+// components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+
 const Home = () => {
   return (
     <section className="h-full">
@@ -10,7 +14,7 @@ const Home = () => {
           <div className="text-center xl:text-left">
             <span className="text-xl">Student Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Henrik Au-Young</span>
+              Hello I&apos;m <br /> <span className="text-accent">Henrik <br />Au-Young</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               Aspiring software developer; proficient in a range of programming languages and technologies.
@@ -25,11 +29,16 @@ const Home = () => {
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
               </Button>
-              <div>socials</div>
+              <div className="mb-8 xl:mb-0"></div>
+              <Social
+                containerStyles={"flex gap-6"}
+                iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
